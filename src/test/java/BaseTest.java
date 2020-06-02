d3a5d21a5d485ca0f4102d8b9b92f1369705a80b
adamzopcsak <adamzopcsak@gmail.com>
2020-06-02 10:40:20 +0200

    BaseTest | class created

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInstance;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class BaseTest {
+
+    protected String username;
+    protected String password;
+    protected ChromeDriver driver;
+
+    @BeforeAll
+    public void setupTestEnvironment() {
+        this.username = System.getenv("DOJOTRACKER_USERNAME");
+        this.password = System.getenv("DOJOTRACKER_PASSWORD");
+    }
+
+    @BeforeEach
+    public void initDriver() {
+        
+    }
+
+}
