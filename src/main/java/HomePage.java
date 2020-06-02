d79779c5b49af659611305ca64ff1bc3db1bebd4
adamzopcsak <adamzopcsak@gmail.com>
2020-06-02 12:13:16 +0200

    HomePage class created

---
+++ src/main/java/HomePage.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage extends Page {
+
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+    
+    public void load() {
+        driver.get(baseUrl);
+    }
+}
