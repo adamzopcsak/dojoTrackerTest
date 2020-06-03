dc2f8bd4bfc81949aefefd6dfe97ca7edc1f464b
adamzopcsak <adamzopcsak@gmail.com>
2020-06-03 12:51:00 +0200

    LoginModal | nextBtn findBy added, fillPasswordField(), login() and getIndexOfLastElement() created

--- src/main/java/LoginModal.java
+++ src/main/java/LoginModal.java
@@ -1,11 +1,20 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.util.List;
+import java.util.Set;
 
 public class LoginModal extends Page {
 
     @FindBy(css = "input[type='email']")
     private WebElement emailInputField;
+
+    @FindBy(css = "span[class='RveJvd snByac']")
+    private WebElement nextBtn;
+
 
     public LoginModal(WebDriver driver) {
         super(driver);
@@ -15,4 +24,25 @@ public class LoginModal extends Page {
         writeToField(emailAddress, emailInputField);
     }
 
+    public void fillPasswordField(String password, WebElement passwordInputField) {
+        writeToField(password, passwordInputField);
+    }
+
+    public void login(String emailAddress, String password) {
+        Set<String> openedWindowsSet = driver.getWindowHandles();
+        Object[] openedWindowsList = openedWindowsSet.toArray();
+        driver.switchTo().window(openedWindowsList[getIndexOfLastElement(openedWindowsList)].toString());
+        fillEmailField(emailAddress);
+        clickOn(nextBtn);
+
+        By passwordFieldLocator = By.cssSelector("input[type='password']");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(passwordFieldLocator));
+        WebElement passwordInputField = driver.findElement(passwordFieldLocator);
+        fillPasswordField(password, passwordInputField);
+        clickOn(nextBtn);
+    }
+    
+    private int getIndexOfLastElement(Object[] array) {
+        return array.length - 1;
+    }
 }
