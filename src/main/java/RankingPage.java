20c1ed9c215bf2bf4487feef71250368703f0830
adamzopcsak <adamzopcsak@gmail.com>
2020-06-04 12:07:41 +0200

    RankingPage | code formatting

--- src/main/java/RankingPage.java
+++ src/main/java/RankingPage.java
@@ -1,7 +1,13 @@
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
 
 public class RankingPage extends Page {
+
     public RankingPage(WebDriver driver) {
         super(driver);
     }
+
 }
