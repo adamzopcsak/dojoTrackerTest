c076bdf3c96c34aaed8b4314f9cb9a92d552d540
adamzopcsak <adamzopcsak@gmail.com>
2020-06-15 13:13:23 +0200

    RankingPage | selector for user panels added, areUsersVisible() created

--- src/main/java/RankingPage.java
+++ src/main/java/RankingPage.java
@@ -1,9 +1,20 @@
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
 
 public class RankingPage extends Page {
 
+    @FindBy(className = "sc-Axmtr")
+    List<WebElement> allUserPanels;
+
     public RankingPage(WebDriver driver) {
         super(driver);
+    }
+
+    public boolean areUsersVisible() {
+        return allUserPanels.size() > 0;
     }
 
 }
