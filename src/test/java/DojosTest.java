085cdd7e3a4b84992d0cb6cebd3287d200fa97a4
adamzopcsak <adamzopcsak@gmail.com>
2020-06-18 11:27:50 +0200

    DojosTest | BeaforeEach was created

--- src/test/java/DojosTest.java
+++ src/test/java/DojosTest.java
@@ -1,5 +1,13 @@
+import org.junit.jupiter.api.BeforeEach;
+
 public class DojosTest extends BaseTest {
 
-    
+    @BeforeEach
+    public void loginAndNavigateToDojosPage() {
+        homePage.login(this.email, this.password);
+        homePage.navigateToDojosPage();
+    }
+
+
 
 }
